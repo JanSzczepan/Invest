7157167f0ebd3f23de25603e9259348fccaba2bc
Jan Szczepan <janszczepan62@gmail.com>
2024-01-23 11:22:07 +0100

    Create instrument service

---
+++ Invest/Services/InstrumentService.cs
@@ -0,0 +1,13 @@
+﻿using Invest.Domain.Entities;
+using Invest.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Invest.Services;
+
+public class InstrumentService(BusinessDbContext context)
+{
+    public async Task<List<Instrument>> GetAllInstrumentsAsync()
+    {
+        return await context.Instruments.ToListAsync();
+    }
+}
