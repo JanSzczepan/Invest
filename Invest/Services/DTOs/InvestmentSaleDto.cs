5bc0fba19a7c15ef1e48d3603ff3821a3cc8ea89
Jan Szczepan <janszczepan62@gmail.com>
2024-01-30 21:09:05 +0100

    Implement functionality to edit investment

---
+++ Invest/Services/DTOs/InvestmentSaleDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Invest.Services.DTOs;
+
+public class InvestmentSaleDto
+{
+    public Guid InvestmentId { get; set; }
+    public DateTime SaleDate { get; set; }
+    public decimal Amount { get; set; }
+    public decimal SalePrice { get; set; }
+}
