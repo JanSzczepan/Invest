7a83c4a0840c20f6f565cf844a3c61eb9a16fe23
Jan Szczepan <janszczepan62@gmail.com>
2024-01-30 19:23:09 +0100

    Add purchase date and fix link url

--- Invest/Services/DTOs/InvestmentDetailsDto.cs
+++ Invest/Services/DTOs/InvestmentDetailsDto.cs
@@ -2,6 +2,8 @@
 
 public class InvestmentDetailsDto
 {
+    public Guid Id { get; set; }
+    public DateTime PurchaseDate { get; set; }
     public string InstrumentTicker { get; set; }
     public string InstrumentName { get; set; }
     public decimal InitialAmount { get; set; }
